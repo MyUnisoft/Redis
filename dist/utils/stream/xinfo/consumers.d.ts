c372bd4f9b75d47f4e9bedf3b506bb90d05be5bc
Nicolas Hallaert <n.hallaert@myunisoft.fr>
2024-08-30 17:28:25 +0200

    chore(): removed dist

--- dist/utils/stream/xinfo/consumers.d.ts
+++
@@ -1,8 +0,0 @@
-export type XINFOConsumers = (string | number)[][];
-export interface XINFOConsumerData {
-    name: string;
-    pending: number;
-    idle: number;
-}
-export declare function parseXINFOConsumers(consumers: XINFOConsumers): XINFOConsumerData[];
-//# sourceMappingURL=consumers.d.ts.map
