9e656b45a76320f9795fa90e88a053ef1993e013
Nicolas Hallaert <n.hallaert@myunisoft.fr>
2024-09-04 17:05:22 +0200

    test(./test/class/Connection): better ut

--- test/class/Connection.spec.ts
+++ test/class/Connection.spec.ts
@@ -9,35 +9,45 @@ import {
 
 describe("Connection", () => {
   describe("Default instantiation", () => {
-    let connection: Connection;
-
-    before(async() => {
-      connection = new Connection({
+    test("Should be well instantiated", async() => {
+      const connection = new Connection({
         port: Number(process.env.REDIS_PORT),
         host: process.env.REDIS_HOST
       });
-    });
 
-    after(async() => {
+      assert.equal(connection.isAlive, false);
+
       await connection.close();
     });
 
-    test("Should be well instantiated", () => {
-      assert.equal(connection.isAlive, false);
-    });
-
     test("Calling Initialize, it should set the ready property at true", async() => {
+      const connection = new Connection({
+        port: Number(process.env.REDIS_PORT),
+        host: process.env.REDIS_HOST
+      });
+
       const res = await connection.initialize();
 
       assert.equal(res.ok, true);
       assert.equal(connection.isAlive, true);
+
+      await connection.close();
     });
 
     test("Calling getConnectionPerf while connection is init", async() => {
+      const connection = new Connection({
+        port: Number(process.env.REDIS_PORT),
+        host: process.env.REDIS_HOST
+      });
+
+      await connection.initialize();
+
       const res = await connection.getConnectionPerf();
 
       assert.equal(res.isAlive, true);
       assert.ok(res.perf! > 0);
+
+      await connection.close();
     });
   });
 });
